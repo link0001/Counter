9ee8d232d6071f1ca4cde53658c068f265524557
lingan <lingan@yahoo.com>
2013-12-23 22:04:08 -0800

    Removed all JQuery. Now simplier and only using Angular

--- counter.js
+++ counter.js
@@ -6,21 +6,20 @@
 var counterApp = angular.module('counterApp', []);
 
 counterApp.controller('pageCtrl', function($scope){
-    $scope.newCounter = function(){
-        // Load an element that uses controller Ctrl
-        $('<div class="singleCounter" ng-controller="counterCtrl"><button id="subtract-btn" ng-click="subtract()">-</button>{{count}}<button id="add-btn" ng-click="add()">+</button></div>').appendTo('body');
-        angular.bootstrap($('.singleCounter'), ['counterApp']);
+    $scope.items=[];
+    $scope.newCounter=function() {
+        $scope.items.push({});
     };
 });
 
 counterApp.controller('counterCtrl', function($scope){
     $scope.count = 0;
 
-    $scope.add = function(){
+    $scope.add = function() {
         $scope.count++;
     };
 
-    $scope.subtract = function(){
+    $scope.subtract = function() {
         $scope.count--;
     };
 });
